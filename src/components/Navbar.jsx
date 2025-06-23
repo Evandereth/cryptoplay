480504b7cd944a02df88bae6051f7d4e411cafb0
evandereth <evandereth@gmail.com>
2025-06-23 22:14:46 +0100

    updating media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import "../css/navbar.css"
 import logo from '../assets/logo.png'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 
 function Navbar(){
     return(
@@ -9,15 +11,23 @@ function Navbar(){
                     <img src={logo} alt="" />
                     <h1>CryptoPlay</h1>
                 </div>
-                <ul>
-                    <li><a href="">about us</a></li>
-                    <li><a href="">cryptocurrency education</a></li>
-                    <li><a href="">cryptocurrency market</a></li>
-                    <li><a href="">faq</a></li>
-                </ul>
-                <div className="btn">
-                    <button>sign up</button>
-                    <button className="active">sign in</button>
+
+                <div className="content">
+                        <ul>
+                        <li><a href="">about us</a></li>
+                        <li><a href="">cryptocurrency education</a></li>
+                        <li><a href="">cryptocurrency market</a></li>
+                        <li><a href="">faq</a></li>
+                    </ul>
+                    <div className="btn">
+                        <button>sign up</button>
+                        <button className="active">sign in</button>
+                    </div>
+                </div>
+                
+                <div className="icon">
+                    <GiHamburgerMenu className="menu show"/>
+                    <FaTimes className="menu cancel"/>
                 </div>
             </nav>
         </>
